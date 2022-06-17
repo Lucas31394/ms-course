1193571c81e3a77633e654d2095925d97b45f17b
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2022-06-17 09:42:28 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/mscourse/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.mscourse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mscourse.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
